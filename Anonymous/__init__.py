3e2da70fcac722db3f3f4eff9a2680928d4bad12
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 13:27:28 +0545

    ㅤㅤ

---
+++ Anonymous/__init__.py
@@ -0,0 +1,9 @@
+from .config import BOT_TOKEN, API_ID, API_HASH
+from pyrogram import Client
+
+app = Client(
+    "bot_session",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
