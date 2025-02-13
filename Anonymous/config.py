30cfa44f57b52e377af30ee102a25e11f7214e7b
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 13:32:28 +0545

    ㅤㅤ

--- Anonymous/config.py
+++ Anonymous/config.py
@@ -1,10 +1,8 @@
-
-class Config:
-    API_ID = 24683098  # Replace with your API ID
-    API_HASH = "e4055cd239464e50e69bd73057c05dd3"  # Replace with your API Hash
-    BOT_TOKEN = "7366616792:AAGGrRIAhvS3v_BOgWwTccRGLpaTKljkPEg"  # Replace with your Bot Token
-    MONGO_URI = "mongodb+srv://herobh123456:hasnainkk07@hasnainkk07.uqjekii.mongodb.net/?retryWrites=true&w=majority"  # Replace with your MongoDB URI
-    ADMINS = [
+API_ID = 24683098  # Replace with your API ID
+API_HASH = "e4055cd239464e50e69bd73057c05dd3"  # Replace with your API Hash
+BOT_TOKEN = "7366616792:AAGGrRIAhvS3v_BOgWwTccRGLpaTKljkPEg"  # Replace with your Bot Token
+MONGO_URI = "mongodb+srv://herobh123456:hasnainkk07@hasnainkk07.uqjekii.mongodb.net/?retryWrites=true&w=majority"  # Replace with your MongoDB URI
+ADMINS = [
         6346273488,  # Me
         1805959544,  # King of Ga*ds (Sukuna)
         1284920298,  # King of Ra*dis (Romeo Randi)
