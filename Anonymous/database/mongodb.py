bdccc732522170f7e628d1d1ffd3d4e6e1cc090b
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 14:45:54 +0545

    ㅤㅤ

--- Anonymous/database/mongodb.py
+++ Anonymous/database/mongodb.py
@@ -1,21 +1,7 @@
-from pymongo import MongoClient
-from Anonymous.config import MONGO_URI, DB_NAME
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URI
 
-class Database:
-    def __init__(self):
-        self.client = MongoClient(MONGO_URI)
-        self.db = self.client[DB_NAME]
-        self.channels = self.db["channels"]
-    
-    def add_channel(self, chat_id, name, username):
-        self.channels.update_one(
-            {"chat_id": chat_id},
-            {"$set": {"name": name, "username": username}},
-            upsert=True
-        )
-
-    def get_channels(self):
-        return list(self.channels.find())
-
-    def remove_channel(self, chat_id):
-        self.channels.delete_one({"chat_id": chat_id})
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["AnonymousBot"]
+channels_col = db["channels"]
+posts_col = db["posts"]
