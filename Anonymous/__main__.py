5bc4d17537247fc3ced9045e18019d5d70530c75
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 14:09:39 +0545

    ㅤㅤ

--- Anonymous/__main__.py
+++ Anonymous/__main__.py
@@ -1,20 +1,7 @@
 # (©) Anonymous Emperor
 
 from Anonymous import app
-import time
-from pyrogram.errors import BadMsgNotification
 
-async def start_bot():
-    retries = 5
-    while retries > 0:
-        try:
-            await app.start()  # This starts the bot
-            break
-        except BadMsgNotification as e:
-            print(f"Error: {e}. Retrying in 5 seconds...")
-            time.sleep(5)
-            retries -= 1
-    if retries == 0:
-        print("Failed to sync time after several retries.")
 
-app.run(start_bot())
+
+app.run()
