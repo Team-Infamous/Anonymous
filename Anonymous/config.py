92b180809d6b7891f26aa3f5cd2ed13406229419
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 13:30:35 +0545

    ㅤㅤ

---
+++ Anonymous/config.py
@@ -0,0 +1,15 @@
+
+class Config:
+    API_ID = 24683098  # Replace with your API ID
+    API_HASH = "e4055cd239464e50e69bd73057c05dd3"  # Replace with your API Hash
+    BOT_TOKEN = "7366616792:AAGGrRIAhvS3v_BOgWwTccRGLpaTKljkPEg"  # Replace with your Bot Token
+    MONGO_URI = "mongodb+srv://herobh123456:hasnainkk07@hasnainkk07.uqjekii.mongodb.net/?retryWrites=true&w=majority"  # Replace with your MongoDB URI
+    ADMINS = [
+        6346273488,  # Me
+        1805959544,  # King of Ga*ds (Sukuna)
+        1284920298,  # King of Ra*dis (Romeo Randi)
+        5907205317,  # Karma Boss 
+        5881613383,  # Anox Sir
+        1963465056, #BABY 
+        8060386745, # my new
+    ]  # Replace with the owner IDs
