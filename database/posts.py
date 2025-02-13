bff37850163b49a175b447a21b50446b11a181a5
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 15:01:18 +0545

    ㅤㅤ

--- database/posts.py
+++ database/posts.py
@@ -3,7 +3,11 @@ from .mongodb import db
 posts_col = db["posts"]
 
 async def save_post(channel_id: int, message_id: int, content: dict):
-    await posts_col.insert_one({"channel_id": channel_id, "message_id": message_id, "content": content})
+    await posts_col.insert_one({
+        "channel_id": channel_id, 
+        "message_id": message_id, 
+        "content": content
+    })
 
 async def get_post(message_id: int):
     return await posts_col.find_one({"message_id": message_id})
