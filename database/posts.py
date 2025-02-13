1ccbed0a8276da7f4afcbae832a360725303eb88
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 15:38:22 +0545

    ㅤㅤ

--- database/posts.py
+++ database/posts.py
@@ -1,13 +1,13 @@
-from .mongodb import db
+from database.mongodb import db
 
 posts_col = db["posts"]
 
 async def save_post(channel_id: int, message_id: int, content: dict):
-    await posts_col.insert_one({
-        "channel_id": channel_id, 
-        "message_id": message_id, 
-        "content": content
-    })
+    await posts_col.update_one(
+        {"message_id": message_id},
+        {"$set": {"channel_id": channel_id, "content": content}},
+        upsert=True
+    )
 
 async def get_post(message_id: int):
     return await posts_col.find_one({"message_id": message_id})
