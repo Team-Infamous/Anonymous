bd20a3c059aa5d59a30eb940add6d36aa25c5683
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-02-13 13:53:58 +0545

    ㅤㅤ

--- Anonymous/plugins/url_buttons.py
+++ Anonymous/plugins/url_buttons.py
@@ -1,5 +1,7 @@
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from Anonymous import app as Client
+
 
 @Client.on_callback_query(filters.regex("^add_url_button_"))
 async def add_url_buttons(client, callback_query):
